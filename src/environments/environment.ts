e56179826a85378b6c148f5161c73073af8203c9
JhoanGon <30383396+JhoanGon@users.noreply.github.com>
2023-04-14 22:56:26 -0500

    Actualizar environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://spidermanapi.us-east-1.elasticbeanstalk.com/spidermans}';
+  baseUrl: 'http://spidermanapi.us-east-1.elasticbeanstalk.com/spidermans';
 
