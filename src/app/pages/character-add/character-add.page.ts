2c56398da7730e8d4995eb25bd14d86bf89e5eda
JhoanGon <30383396+JhoanGon@users.noreply.github.com>
2023-04-22 12:11:38 -0500

    Update form add characters

--- src/app/pages/character-add/character-add.page.ts
+++ src/app/pages/character-add/character-add.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SpiderServiceService } from 'src/app/services/spider-service.service';
 import { HttpClient } from '@angular/common/http';
 import { AuthServiceService } from 'src/app/services/auth-service.service';
+import * as AWS from 'aws-sdk';
 
 @Component({
   selector: 'app-character-add',
@@ -19,6 +20,10 @@ export class CharacterAddPage implements OnInit {
   genderValue: string;
   raceValue: string;
   descriptionValue: string;
+  isValid: boolean = true;
+  base64String: string;
+  fileName: string;
+
 
   constructor(private spiderService: SpiderServiceService, private http: HttpClient, public authService: AuthServiceService) {}
 
@@ -27,10 +32,44 @@ export class CharacterAddPage implements OnInit {
   splitWord(word: string): string[] {
     return word.split('');
   }
+  async uploadToS3(base64String: string, fileName: string) {
+    const bucket = new AWS.S3({
+      accessKeyId: 'AKIAQC4GJNHKNELBYXDY',
+      secretAccessKey: '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',
+      region: 'us-east-1'
+    });
+
+    const params = {
+      Bucket: 'spidermansphotos',
+      Key: 'spidy/' + fileName,
+      Body: new Uint8Array(atob(base64String).split('').map(char => char.charCodeAt(0))),
+      ContentType: 'image/webp' // Change according to the file type
+    };
+
+    try {
+      const s3Response = await bucket.upload(params).promise();
+      console.log('Data:', s3Response);
+      this.imgValue = s3Response.Location; // Update image variable with S3 URL
+    } catch (err) {
+      console.log('Error:', err);
+    }
+  }
+
+  onFileSelected(event : any) {
+    const file: File = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      if (reader.result != null) { // Add null check
+        this.base64String = reader.result.toString().split(',')[1];
+        this.fileName = file.name;
+      }
+    };
+  }
 
   onSubmit() {
     //console.log(this.nameValue);
-    const newSpider = {
+    const newSpider: any = {
       name: this.nameValue,
       alias: this.aliasValue,
       earth: this.earthValue,
@@ -43,10 +82,21 @@ export class CharacterAddPage implements OnInit {
     };
 
     console.log(newSpider);
-
-    this.spiderService.postSpider(newSpider).subscribe((response) => {
-      console.log(response);
+    Object.keys(newSpider).forEach(key => {
+      if (!newSpider[key]) {
+        this.isValid = false;
+        return;
+      }
     });
+
+    if (this.isValid) {
+      this.uploadToS3(this.base64String,this.fileName);
+      this.spiderService.postSpider(newSpider).subscribe((response) => {
+        console.log(response);
+      });
+    } else {
+      alert('Por favor complete todos los campos obligatorios');
+    }
   }
 
 }
