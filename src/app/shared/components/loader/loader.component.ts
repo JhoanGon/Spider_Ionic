99567fe79e25033e7e3dce843de0165c6bfbefd6
JhoanGon <30383396+JhoanGon@users.noreply.github.com>
2023-04-22 01:40:05 -0500

    optimized loder

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss'],
+  styleUrls: ['./loader.component.scss', './loader2.component.scss'],
 })
 export class LoaderComponent  implements OnInit {
 
